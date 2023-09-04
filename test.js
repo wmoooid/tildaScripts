4f09721c7804efbfd3510b221b15b1cd98de877e
wmoooid <threeqletters@yandex.ru>
2023-09-04 17:58:30 +0300

    color-picker

--- test.js
+++ test.js
@@ -1,9 +1,24 @@
-$(document).ready(() => {
-  $('.t744__btn-wrapper').each((i, el) => {
-    $(el).prepend(`<a class="button-accent" href="${$(el).closest('.t-container').data('product-url')}">Узнать подробнее</a>`);
-  });
+$('.t-slds__thumbsbullet .t-slds__bgimg').each((i, el) => {
+    const src = el.dataset.original;
 
-  $('.t744 .js-product-option').each((i, el) => {
-    el.style.setProperty('--bg-image', `url(${$(el).find('option').text()})`);
-  });
+    const canvas = document.createElement('canvas');
+
+    const pic = new Image();
+    pic.crossOrigin = 'Anonymous';
+    pic.src = src;
+    pic.onload = function () {
+        canvas.width = pic.width;
+        canvas.height = pic.height;
+        var ctx = canvas.getContext('2d');
+
+        ctx.drawImage(pic, 0, 0);
+
+        var c = canvas.getContext('2d');
+        var p = c.getImageData(110, 130, 1, 1).data;
+        var hex = 'rgb(' + p[0] + ', ' + p[1] + ', ' + p[2] + ')';
+
+        console.log(c.getImageData(110, 130, 1, 1));
+
+        el.style.backgroundColor = hex;
+    };
 });
